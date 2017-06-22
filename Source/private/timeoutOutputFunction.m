32adcef8905804cfc6869c696f77310a93ce27d5
dflowers7 <dflowers@mit.edu>
2017-06-22 14:31:30 -0400

    Fixed integration timeout function
    Function was written according to the specification for fmincon output functions and therefore wasn't working for calls to ode15sf.

--- Source/private/timeoutOutputFunction.m
+++ Source/private/timeoutOutputFunction.m
@@ -22,11 +22,20 @@ fun = @outputfun;
             case 'init'
                 % Start timer at beginning of integration
                 thistic = tic;
+            case ''
+                % Stop if time elapsed exceeds the timeout duration
+                thisstop = toc(thistic) > secondsToTimeout;
             case 'iter'
                 % Stop if time elapsed exceeds the timeout duration
                 thisstop = toc(thistic) > secondsToTimeout;
         end
-        
+    
+        % Use an assertion to issue an error here, since setting the stop
+        % flag only stops integration as an event. If the integration would
+        % have stopped anyway, don't issue an error.
+        assert(~thisstop || stop, 'KroneckerBio:timeoutOutputFunction:IntegrationTimeout', ...
+            'A single ODE solver call took longer than %g seconds', secondsToTimeout)
+
         % Stop if timeout applies or if other output function issues a stop
         % condition
         stop = thisstop || stop;
